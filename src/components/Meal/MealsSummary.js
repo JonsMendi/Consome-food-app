1cb191496e5443d4244219a2f08812776515dd3a
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-08-02 15:50:04 +0200

    created component that contains welcome text.
    If applied, this commit will have created component that contains welcome text.

---
+++ src/components/Meal/MealsSummary.js
@@ -0,0 +1,15 @@
+import styles from "./MealsSummary.module.css";
+
+const MealsSummary = (props) => {
+  return (
+    <div className={styles.summary}>
+      <h2>"Consommé or Consommé?"</h2>
+      <p>
+        Choose the best, almost extinct vital products to keep your pleasure and
+        joy above anything else.
+      </p>
+      <p>Spend your fortune with us, Consommé!</p>
+    </div>
+  );
+};
+export default MealsSummary;
