43344fcb2884e1766ef6603e0e41796024ddb449
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-08-04 15:41:42 +0200

    added Cart modal functionality.
    If applied this commit will have added the Cart modal functionality.

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,18 +1,30 @@
-import { Fragment } from "react";
-import MealImage from "../../../assets/pexelstoaheftibaşinca.jpg";
+import React, { Fragment, useState } from "react";
+import MealImage from "../../../assets/pexelstoaheftibaşinca.png";
+import Cart from "../../Cart/Cart";
 import ButtonHeader from "../../UI/ButtonHeader";
 import styles from "./Header.module.css";
 
 const Header = (props) => {
+  const [openModal, setOpenModal] = useState(false);
+
+  const openModalHandler = (event) => {
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
   return (
     <Fragment>
       <header className={styles.header}>
         <h1>Consommé-mé</h1>
-        <ButtonHeader></ButtonHeader>
+        <ButtonHeader onClick={openModalHandler}></ButtonHeader>
       </header>
       <div className={styles["main-image"]}>
         <img src={MealImage} alt="meal representational" />
       </div>
+      {openModal && <Cart onCloseModal={handleCloseModal} />}
     </Fragment>
   );
 };
