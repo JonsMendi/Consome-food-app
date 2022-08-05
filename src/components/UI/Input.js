de91b3d175a78ff931170ce3d2814e11af7f52ca
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-08-05 15:27:22 +0200

    added forwardRef to allow the Input component the use of refs

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,11 +1,12 @@
+import React from "react";
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input {...props.input} onChange={props.onChange} />
+      <input ref={ref} {...props.input} onChange={props.onChange} />
     </div>
   );
-};
+});
 export default Input;
