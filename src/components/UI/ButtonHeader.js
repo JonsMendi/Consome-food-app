f158a4596d6074f17446242aba3925bd57d5a31f
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-08-24 14:37:20 +0200

    add style and functionality to make cart button "bumps" any time an article is added to the cart.
    If applied, this commit will have add style and functionality to make cart button "bumps" any time an article is added to the cart.

--- src/components/UI/ButtonHeader.js
+++ src/components/UI/ButtonHeader.js
@@ -1,20 +1,42 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import classes from "./ButtonHeader.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 
 const ButtonHeader = (props) => {
+  const [bumping, setBumping] = useState(false);
   const ctx = useContext(CartContext);
 
-  const numberOfCartItems = ctx.items.reduce((curNumber, item) => {
+  //destructuring the CartContext
+  const { items } = ctx;
+
+  //to add more more items in the cart.
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
+
+  const cartBumpingClass = `${classes.button} ${bumping ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBumping(true);
+
+    const bumpTimer = setTimeout(() => {
+      setBumping(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(bumpTimer);
+    };
+  }, [items]);
 
   return (
     <button
       onClick={props.onClick}
       type={props.type || "button"}
-      className={classes.button}
+      className={cartBumpingClass}
     >
       <span className={classes.icon}>
         <CartIcon />
